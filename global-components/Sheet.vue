a48ed26f5df063c694266d1f47e83b69b60be56d
liamdebellada <liam.debell@ada.ac.uk>
2020-11-27 00:08:20 +0000

    Fixed bottom sheet component with reactive rendering, and added a scroll component for viewing requested data. Also added additional snap points to animated sheet

---
+++ global-components/Sheet.vue
@@ -0,0 +1,27 @@
+<template>
+    <scroll-view class="sheet-container">
+        <text class="data-title">{{item.title}}</text>
+        <text>Minerals are cute.</text>
+    </scroll-view>
+</template>
+
+<script>
+export default {
+    props: {
+        item: { type: Object }
+    }
+}
+</script>
+
+<style>
+.sheet-container {
+    height: 450;
+    background-color: white;
+    padding: 10;
+}
+
+.data-title {
+    font-size: 20;
+    font-weight: 500;
+}
+</style>
--- router/Map.vue
+++ router/Map.vue
@@ -4,18 +4,19 @@
             <map-view class="container" :initial-region="coordinates">
                 <Marker :coordinate="{latitude: 51.5858233, longitude: -0.0658707}" :onPress="() => createSlideView()">
                     <view class="marker">
-                         <text class="marker-text">Ada national college for digital skills</text>
+                         <image :source="{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Coelestin_01.jpg/1200px-Coelestin_01.jpg'}" class="marker-img"/>
                     </view>
                 </Marker>
             </map-view>
             <BottomSheet
             ref="refer"
-            :snapPoints="[330, 90]"
+            :snapPoints="[660, 330, 90]"
             :borderRadius="0"
             :renderContent="renderContent"
             :renderHeader="renderHeader"
-            :initialSnap="1"
+            :initialSnap="2"
             :enableGestureInteraction="true"
+            :enabledInnerScrolling="false"
             />
         </view>
         <NavBar :navigation="this.props.navigation"></NavBar>
@@ -31,23 +32,23 @@ import Animated from 'react-native-reanimated';
 import BottomSheet from 'reanimated-bottom-sheet';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-const renderContent = () => {
+import SheetView from '../global-components/Sheet.vue'
+
+var data = {
+    title: "Content title"
+}
+var renderContent = () => {
     return (
-        <View style={{backgroundColor: 'white', padding: 16, height: 450}}>
-            <Text>test</Text>
-            <Text>OMG :)</Text>
-            <Text>OMG :)</Text>
-            <Text>OMG :)</Text>
-            <Text>OMG :)</Text>
-            <Text>OMG :)</Text>
-            <Text>OMG :)</Text>
+        <View style={{backgroundColor: 'white', height: 1000}}>
+            <SheetView item={data}/>
         </View>
     )
 };
 
 const renderHeader = () => {
     return (
-        <View style={{backgroundColor: 'red', padding: 16, height: 40, borderTopLeftRadius: 10, borderTopRightRadius: 10}}>
+        <View style={{backgroundColor: 'white', padding: 16, height: 40, borderTopLeftRadius: 10, borderTopRightRadius: 10, flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <View style={{backgroundColor: "#525252", height: 15, width: 70, borderRadius: 30}}></View>
         </View>
     )
 }
@@ -71,7 +72,12 @@ export default {
     components: { NavBar, MapView, Marker, BottomSheet },
     methods: {
         createSlideView() {
-            this.$refs.refer.snapTo(0)
+            this.$refs.refer.snapTo(1)
+            // this.$refs.refer.props.renderContent = renderContent()
+            data = {
+                title: "Minerals"
+            }
+            this.$refs.refer.forceUpdate()
         }
     }
 }
@@ -88,9 +94,10 @@ export default {
 
 .marker {
     background-color: #171717;
-    min-width: 50;
+    min-width: 65;
     width: auto;
-    height: 25;
+    height: 65;
+    border-radius: 50;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -98,5 +105,11 @@ export default {
 
 .marker-text {
     color: white;
+}
+
+.marker-img {
+    height: 50;
+    width: 50;
+    border-radius: 15;
 }
 </style>
